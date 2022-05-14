38fcebecd5c40072ff172a125e1867da57d70d25
zohaib arshad <zohaibarshad2237@gmail.com>
2022-05-15 04:38:32 +0500

    added logout

--- src/components/title/title.js
+++ src/components/title/title.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Button from '../button/button';
 
 const Title = (props) => {
-
   return (
     <div className="header-container">
-      <div>
-        {props.title}
-      </div>
-        
+      <div>{props.title}</div>
+      <Button
+        label={'Log Out'}
+        onConfirm={() =>
+          (window.location.href =
+            'https://ggd-employee-identities-dev.auth.us-east-2.amazoncognito.com/login?client_id=5dncsjfvgeuu9qmot7rgg9o202&response_type=code&scope=email+openid+profile&redirect_uri=http://localhost:8080')
+        }
+        classname="primary-btn"
+        type="button"
+      />
     </div>
-)};
+  );
+};
 
 Title.defaultProps = {
   title: 'Json Rule Editor',
@@ -18,6 +25,6 @@ Title.defaultProps = {
 
 Title.propTypes = {
   title: PropTypes.string,
-}
+};
 
 export default Title;
